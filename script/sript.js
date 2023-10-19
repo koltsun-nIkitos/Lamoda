e3eaec1e61912795088ffd91b7b045ad96c807b1
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-10-20 01:00:19 +0300

    v2.0.0

--- script/sript.js
+++ script/sript.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const headerCityButton = document.querySelector(".header__city-button");
-
+const subheaderCart = document.querySelector(".subheader__cart");
+const cartOverlay = document.querySelector(".cart-overlay");
 
 headerCityButton.textContent = localStorage.getItem("Lomoda-location") || "Ваш город?";
 
@@ -11,3 +12,80 @@ headerCityButton.addEventListener("click", () => {
     headerCityButton.textContent = city;
     localStorage.setItem("Lomoda-location", city)
 })
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+    document.body.dbScrollY = window.scrollY;
+
+    document.body.style.cssText = `
+        position: fixed;
+        top: -${window.scrollY}px;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        padding-right: ${widthScroll}px;
+    `;
+}
+
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY,
+    })
+};
+
+
+// Модальное окно
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
+}
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
+}
+
+
+
+
+// Получение БД
+
+const getData = async () =>{
+    const data = await fetch('db.json')
+
+    if (data.ok){
+        return data.json();
+    } else {
+        throw new Error(`Данные не были получены. Ошибка: ${data.status} ${data.statusText}`)
+    }
+}
+
+const getGoods = (callback) =>{
+    getData()
+        .then((data) => {
+            callback(data);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+}
+
+getGoods((data) => {
+    console.log(data)
+})
+
+
+
+subheaderCart.addEventListener('click', () => {
+    cartModalOpen();
+});
+
+cartOverlay.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('cart__btn-close') || target.matches('.cart-overlay')){
+        cartModalClose();
+    }
+});
